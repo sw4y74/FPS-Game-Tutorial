4ac6b1af3532c9fe76d886a2201d873d20c55276
sw4y74 <85453219+sw4y74@users.noreply.github.com>
2022-06-01 21:41:56 +0200

    meniu

---
+++ FPS Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+    }
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
